4cc21da5434693468957c9de13ac82a83503ac15
João Gabriel Souza Saraiva <joaogabrielssaraiva@gmail.com>
2021-04-02 19:03:35 -0300

    primeira abertura de arquivo
    foi o primeiro passo do projeto, no qual implementei a organização dos arquivos salvos na pasta.

---
+++ Arquivos/GuardarSenha.py
@@ -0,0 +1,38 @@
+"""
+                                Este projeto está sendo desenvolvido inteiramente
+                                Por João Saraiva, e eu disponibilizei este projeto
+                                Para o público, para que o conhecimento se espalhe 
+                                sempre! 
+"""
+""" 
+                    ESTRUTURA FUNCIONAL DE GERAR UM ARQUIVO NA PASTA COM O RELATIONAL PATH
+arquivo = open("Arquivos\SenhasSalvas\senhas.txt",'a')
+arquivo.write("By: Joao saraivx")
+arquivo.close()
+
+"""
+#Rede Social
+print("Para inciarmos você deve digitar o nome da rede social que você quer armazenar a senha.")
+redeSocial = input("\n" "Digite o nome da rede social. ")
+print("")
+
+#Login
+print("Agora você deverá digitar o nome de usuario/email vinculado a conta.")
+print("Como por exemplo: \n"
+"@exemplodeuser ou")
+print("exemplo@email.com.br")
+print("")
+User = input("\n" "Digite o seu nome de usúario. ")
+
+#Senhas
+print("Olá, para começarmos a salvar as senhas, você deverá digitar \n"
+"sua senha ")
+print("")
+
+senhaAserGravada = input("\n" "Digite sua senha! ")
+
+"""
+senhaSalvas = open("Arquivos\SenhasSalvas\senhas.txt",'a')
+senhaSalvas.write(senhaAserGravada, "=" redeSocial)
+senhaSalvas.write("\n")
+"""
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PassManager Project
- este projeto será feito para o desenvolvimento de um gerenciador de senhas do usuario
+ este projeto será feito para o desenvolvimento de um gerenciador de senhas do usuario, ele funcionará com a execução do GuardarSenha.
+ e com isso você dará a entrada devida que você queira.
+
+ Após isso você abrirá o ConsultaSenha para consultar as senhas salvas.
