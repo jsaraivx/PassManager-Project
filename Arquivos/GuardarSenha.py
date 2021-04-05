bba2b03323dfc4b843d188c75a20b9e8e126dd52
João Gabriel Souza Saraiva <joaogabrielssaraiva@gmail.com>
2021-04-05 15:00:38 -0300

    Update

--- Arquivos/GuardarSenha.py
+++ Arquivos/GuardarSenha.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 """
                                 Este projeto está sendo desenvolvido inteiramente
                                 Por João Saraiva, e eu disponibilizei este projeto
@@ -88,32 +89,94 @@ print("\t\tLISTA DE REDES SOCIAIS\n",
  "\t\t Reddit = 11\n",
  "\t\t MiCloud = 12")
 opcaoselecionada = int(input("\t\tOpção Selecionada: "))
-
     #Inicio de captura de daodos.
 if(opcaoselecionada == 1):
-    print("Você selecionou Instagram")
+    opcaoselecionada = 1
+    print("\t\tVocê selecionou: Instagram <=")
+
 elif(opcaoselecionada == 2):
-    print("Você selecionou Facebook")
+    opcaoselecionada = 2
+    print("\t\tVocê selecionou: Facebook <=")
+
 elif(opcaoselecionada == 3):
-    print("Você selecionou GitHub")
+    opcaoselecionada = 3
+    print("\t\tVocê selecionou: GitHub <=")
+
 elif(opcaoselecionada == 4):
-    print("Você selecionou Twitter")
+    opcaoselecionada = 4
+    print("\t\tVocê selecionou: Twitter <=")
+
 elif(opcaoselecionada == 5):
-    print("Você selecionou Google")
+    opcaoselecionada = 5
+    print("\t\tVocê selecionou Google <=")
+
 elif(opcaoselecionada == 6):
-    print("Você selecionou YouTube")
+    opcaoselecionada = 6
+    print("\t\tVocê selecionou: YouTube <=")
+
 elif(opcaoselecionada == 7 ):
-    print("Você selecionou LinkedIn")
+    opcaoselecionada = 7
+    print("\t\tVocê selecionou: LinkedIn <=")
+
 elif(opcaoselecionada == 8):
-    print("Você selecionou TikTok")
+    opcaoselecionada = 8
+    print("\t\tVocê selecionou: TikTok <=")
+
 elif(opcaoselecionada == 9):
-    print("Você selecionou SnapChat")
+    opcaoselecionada = 9
+    print("\t\tVocê selecionou: SnapChat <=")
+
 elif(opcaoselecionada == 10):
-    print("Você selecionou iCloud")
+    opcaoselecionada = 10
+    print("\t\tVocê selecionou: iCloud <=")
+
 elif(opcaoselecionada == 11):
-    print("Você selecionou Reddit")
+    opcaoselecionada = 11
+    print("\t\tVocê selecionou: Reddit <=")
+
 elif(opcaoselecionada == 12):
-    print("Você selecionou MiCloud")
+    opcaoselecionada = 12
+    print("\t\tVocê selecionou: MiCloud <=")
 
+else:
+    print("\t\tO valor que você digitou está errado!\n"
+    "\t\tpor favor reinicie o programa.\n")
+#print("\n\n\t\t", opcaoselecionada)
 
+senhagerenciada = opcaoselecionada
+if senhagerenciada==1:
+    arquivoasersalvo = open("Arquivos\SenhasSalvas\SenhaIntagram.txt",'a')
+
+elif senhagerenciada == 2:
+    arquivoasersalvo = open("Arquivos\SenhasSalvas\SenhaFacebook.txt",'a')
+
+elif senhagerenciada == 3:
+    arquivoasersalvo = open("Arquivos\SenhasSalvas\SenhaGitHub.txt",'a')
+
+elif senhagerenciada == 4:
+    arquivoasersalvo = open("Arquivos\SenhasSalvas\SenhaTwitter.txt",'a')
+
+elif senhagerenciada == 5:
+    arquivoasersalvo = open("Arquivos\SenhasSalvas\SenhaGoogle.txt",'a')
+
+elif senhagerenciada == 6:
+    arquivoasersalvo = open("Arquivos\SenhasSalvas\SenhaYoutube.txt",'a')
+
+elif senhagerenciada == 7:
+    arquivoasersalvo = open("Arquivos\SenhasSalvas\SenhaLinkedIn.txt",'a')
+
+elif senhagerenciada == 8:
+    arquivoasersalvo = open("Arquivos\SenhasSalvas\SenhaTikTok.txt",'a')
+
+elif senhagerenciada == 9:
+    arquivoasersalvo = open("Arquivos\SenhasSalvas\SenhaSnapChat.txt",'a')
+
+elif senhagerenciada == 10:
+    arquivoasersalvo = open("Arquivos\SenhasSalvas\SenhaiCloud.txt",'a')
+
+elif senhagerenciada == 11:
+    arquivoasersalvo = open("Arquivos\SenhasSalvas\SenhaReddit.txt",'a')
+
+elif senhagerenciada == 12:
+    arquivoasersalvo = open("Arquivos\SenhasSalvas\SenhaMiCloud.txt",'a')
 
